1fdc0cd9817fe1df7453b70528a848ecb87652cd
Tom <thomas.verin@sonarsource.com>
2019-10-16 11:03:21 +0200

    Fix public-sync repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,14 +6,14 @@ echo "[user]" > ~/.gitconfig
 echo "	email = sonartech@sonarsource.com" >> ~/.gitconfig
 echo "  name = sonartech" >> ~/.gitconfig
 
-git clone https://${GITHUB_TOKEN}@github.com/public-git-sync.git
+git clone https://${GITHUB_TOKEN}@github.com/SonarSource/public-git-sync.git
 cd public-git-sync
 
 git clone https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git repo
 cd repo
 
 echo "Starting sync"
-../sync_public_master.sh slang https://${GITHUB_TOKEN}@github.com/${GITHUB_TARGET_REPOSITORY}.git
+../sync_public_master.sh slang https://${GITHUB_TOKEN}@github.com/SonarSource/${GITHUB_TARGET_REPOSITORY}.git
 
 echo "Starting push"
 ../commit_sync_public_master.sh ${GITHUB_TARGET_REPOSITORY}
