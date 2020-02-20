cc6eb638d920b1532b559872cd1179184c2ecdf1
sonartech <sonartech@sonarsource.com>
2020-02-20 14:00:00 +0100

    Make action usable for SonarQube

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,18 @@
 set -e
 set -o pipefail
 
-if [ "${GITHUB_REPOSITORY}" == "SonarSource/slang" ];then
+if [[ "${GITHUB_REPOSITORY}" == "SonarSource/slang" || "${GITHUB_REPOSITORY}" == "SonarSource/sonarqube" ]];then
   echo "No sync from public repository"
+  exit 0
+fi
+
+if [ -z "${GITHUB_TARGET_REPOSITORY}" ];then
+  echo "You didn't provide a target repository"
+  exit 0
+fi
+
+if [ -z "${BRANCH_NAME}" ];then
+  echo "You didn't provide a branch name"
   exit 0
 fi
 
@@ -20,7 +30,7 @@ cd repo
 ls
 echo "Starting sync"
 pwd
-../sync_public_branch.sh slang https://${GITHUB_TOKEN}@github.com/SonarSource/${GITHUB_TARGET_REPOSITORY}.git master
+../sync_public_branch.sh ${GITHUB_REPOSITORY} https://${GITHUB_TOKEN}@github.com/SonarSource/${GITHUB_TARGET_REPOSITORY}.git ${BRANCH_NAME}
 
 echo "Starting push"
-../commit_sync_public_branch.sh ${GITHUB_TARGET_REPOSITORY} master
+../commit_sync_public_branch.sh ${GITHUB_TARGET_REPOSITORY} ${BRANCH_NAME}
